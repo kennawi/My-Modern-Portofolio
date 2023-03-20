0d0c61eb40cb0bf46d7f417ca3c27a295b8e9b2c
Ahmed Kennawi <ahmed.kennawi@kendimed.com>
2023-03-20 05:30:56 +0200

    first commit

--- my-portfolio/schemas/index.ts
+++ my-portfolio/schemas/index.ts
@@ -1,9 +1,7 @@
-import blockContent from './blockContent'
-
 import experience from './experience'
 import skill from './skill'
 import social from './social'
 import project from './project'
 import pageInfo from './pageInfo'
 
-export const schemaTypes = [blockContent, experience, skill, social, project, pageInfo]
+export const schemaTypes = [experience, skill, social, project, pageInfo]
